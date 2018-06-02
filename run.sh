ec2c260eb017c209e4671ae89822c454d3cf93e0
Balazs Major <major.balazs@icloud.com>
2018-06-03 00:42:30 +0200

    Fix back.

--- run.sh
+++ run.sh
@@ -13,10 +13,10 @@ ES_URL=$ES_HOST/bloomsky/doc/_bulk?pretty
 
 echo "---------=======----------"
 echo "Getting data from Bloomsky API!"
-Bloomsky_API_intl.sh > $BLOOMSKY_FILE_NAME
+./Bloomsky_API_intl.sh > $BLOOMSKY_FILE_NAME
 
 echo "Parsing data in $BLOOMSKY_FILE_NAME started at $(date +%F_%R)."
-parse-bloomsky-json.sh $BLOOMSKY_FILE_NAME > $ES_FILE_NAME
+./parse-bloomsky-json.sh $BLOOMSKY_FILE_NAME > $ES_FILE_NAME
 
 echo "Upload parsed data in $ES_FILE_NAME started at $(date +%F_%R)."
 curl -H 'Content-Type: application/x-ndjson' -XPOST $ES_URL --data-binary @$ES_FILE_NAME
